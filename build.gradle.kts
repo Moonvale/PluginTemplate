1d003c58d7e27896577058c98b3e800600ef54ac
Lukas <rubymc@gmx.de>
2024-11-29 17:52:00 +0100

    basic structure commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+}
+
+group = "eu.moonvale"
+version = "1.0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+}
+
+kotlin {
+    jvmToolchain(21)
+}
