25930d69a106cfbd97a10228882bd2e4195500ca
Waischbrot <lukas.bollhoefer@dm.de>
2024-12-01 10:06:42 +0100

    current state

--- src/main/kotlin/eu/moonvale/template/TemplatePlugin.kt
+++ src/main/kotlin/eu/moonvale/template/TemplatePlugin.kt
@@ -1,20 +1,23 @@
 package eu.moonvale.template
 
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
 
 /**
  * Main class of the plugin
  */
 class TemplatePlugin : JavaPlugin() {
-
     /**
      * Called when the plugin is enabled
      */
-    override fun onEnable() {}
+    override fun onEnable() {
+        Bukkit.getLogger().info("Plugin enabled successfully!")
+    }
 
     /**
      * Called when the plugin is disabled
      */
-    override fun onDisable() {}
-
+    override fun onDisable() {
+        Bukkit.getLogger().info("Plugin disabled.")
+    }
 }
